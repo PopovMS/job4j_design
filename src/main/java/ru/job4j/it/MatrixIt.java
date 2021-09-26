1a6b0b67dfe8360257e16fcc8e607e4267c084d0
PopovMS <m.popov83@gmail.com>
2021-09-26 08:54:05 +0500

    5.1.1. Итератор для двухмерного массива int[][] [#9539 #198814]. Рефакторинг

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -3,6 +3,10 @@ package ru.job4j.it;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Класс реализует итератор для двухмерного массива
+ */
+
 public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
     private int row = 0;
@@ -12,32 +16,46 @@ public class MatrixIt implements Iterator<Integer> {
         this.data = data;
     }
 
+    /**
+     * Метод проверяет наличие следующего элемента в матрице начиная с текущего,
+     * устанавливает курсор на найденый элемент, если текущий нулевой.
+     * @return возвращает true если элемент найден
+     */
     @Override
     public boolean hasNext() {
-        return search(false) != null;
+        while (row < data.length) {
+            if (data[row].length != 0) {
+                if (data[row][column] != 0) {
+                    return true;
+                }
+            }
+            moveCursor();
+        }
+        return false;
     }
 
+    /**
+     * Метод возвращает текущий элемент на который установлен курсор
+     * и сдвигает курсор на одно значение вперед
+     * @return возвращает текущий элемент матрицы
+     */
     @Override
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return search(true);
+        int result = data[row][column];
+        moveCursor();
+        return result;
     }
-    public Integer search(boolean vol) {
-        for (int r = row; r < data.length; r++) {
-            for (int c = column; c < data[r].length; c++) {
-                if (data[r][c] != 0) {
-                    if (vol) {
-                        column++;
-                        if (column >= data[row].length) {
-                            row++; column = 0;
-                        }
-                    }
-                    return data[r][c];
-                }
-            }
+
+    /**
+     * Метод инкрементирует переменные курсора
+     */
+    public void moveCursor() {
+        column++;
+        if (column >= data[row].length) {
+            row++; column = 0;
         }
-        return null;
     }
 }
