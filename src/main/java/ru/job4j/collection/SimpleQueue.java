01fc62bfb0bd30fabedf7193cf399ca8e2831317
PopovMS <m.popov83@gmail.com>
2021-10-12 13:02:27 +0500

    Очередь на двух стеках [#160 #211406]. Исправление.

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,7 +1,5 @@
 package ru.job4j.collection;
 
-import java.util.NoSuchElementException;
-
 /**
  * Класс описывает организацию данных по алгоритму "FIFO"
  * @param <T> принимает входящий элемент
@@ -9,7 +7,7 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-
+    private int count;
     /**
      * перекладывает элементы из стека in в стек out,
      * извлекает последний элемент из out (бывший первым в in), удаляя его,
@@ -18,21 +16,18 @@ public class SimpleQueue<T> {
      */
     public T poll() {
         T rsl;
-        while (true) {
-            try {
+        int tempCount = count;
+        while (tempCount > 0) {
                 out.push(in.pop());
-            } catch (NoSuchElementException inEx) {
-                rsl = out.pop();
-                while (true) {
-                    try {
-                        in.push(out.pop());
-                    } catch (NoSuchElementException outEx) {
-                        return rsl;
-                    }
-                }
-            }
+                tempCount--;
         }
-
+        rsl = out.pop();
+        count--;
+        while (tempCount < count) {
+            in.push(out.pop());
+            tempCount++;
+        }
+        return rsl;
     }
 
     /**
@@ -41,5 +36,6 @@ public class SimpleQueue<T> {
      */
     public void push(T value) {
         in.push(value);
+        count++;
     }
 }
