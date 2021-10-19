1c89c56eecee8ee14ad53ad40fe66f9d777adb52
PopovMS <m.popov83@gmail.com>
2021-10-19 23:07:00 +0500

    Создать модель User [#999 #218147]

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,28 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Класс описывает модель данных пользователя.
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    /**
+     * Конструктор класса
+     * @param name поле name
+     * @param children поле children
+     * @param birthday поле дата рождения
+     */
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
+
