3e50ad8c5f880275196bfdf4985c1dff076b73ee
PopovMS <m.popov83@gmail.com>
2022-01-21 10:55:09 +0500

    BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,6 +9,11 @@ import java.util.stream.Collectors;
  * Класс реализует поиск значения по файлу со строками
  */
 public class LogFilter {
+    /**
+     * Метод читает файл и фильтрует из него значение переданное с предикатом
+     * @param file принимает имя файла
+     * @return возвращает отфильтрованный лист
+     */
     public static List<String> filter(String file) {
         List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
@@ -22,8 +26,26 @@ public class LogFilter {
         return rsl;
     }
 
+    /**
+     * Метод сохраняет отфильтрованный лист в файл
+     * @param log принимает лист со строками
+     * @param file принимает имя файла, куда нужно записать строки
+     */
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String vol : log) {
+                out.println(vol);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "404.txt");
     }
 }
