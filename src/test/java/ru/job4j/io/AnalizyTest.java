8548e95657bcc28e7a506f72529ea22b5c76205d
PopovMS <m.popov83@gmail.com>
2022-01-26 19:37:13 +0500

    Анализ доступности сервера. [#859 #254701]. Исправление.

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -13,7 +13,17 @@ public class AnalizyTest {
 
     @Test
     public void test() throws IOException {
+        File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
+        try (PrintWriter writerOut = new PrintWriter(source)) {
+            writerOut.println(
+                    "200 10:56:01\n" +
+                    "200 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+        }
         new Analizy().unavailable("./server_log.txt", target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
