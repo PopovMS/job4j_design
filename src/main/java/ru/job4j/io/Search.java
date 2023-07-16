e9081ba2849960a6ac233d5f5a2923b7eb060408
Misha <m.popov83@gmail.com>
2023-07-16 18:47:35 +0500

    Валидация параметров запуска [#246865].Исправление

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,13 +38,20 @@ public class Search {
     }
 
     /**
-     * Метот проверяет количество переданных параметров
+     * Метот проверяет количество переданных параметров и их соответствия требованиям
      * @param args - принимает массив с параметрами
-     * @return - возвращает true, если параметров меньше двух
+     * @return - возвращает true, если параметры соответствуют требованиям
      */
     private static boolean testParam(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Отсутствуют параметры. Используйте 2 параметра.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         return true;
     }
