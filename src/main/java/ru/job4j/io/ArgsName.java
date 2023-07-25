6c6d20ad149d6f3cd25cc173e00bb82aa0332379
Misha <m.popov83@gmail.com>
2023-07-26 01:30:22 +0500

    Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,82 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class ArgsName {
+    /**
+     * Мапа для хранения пароы ключ-значение
+     */
+    private final Map<String, String> values = new HashMap<>();
+
+    /**
+     * Метод проверяет наличие переданного ключа в хэшМап
+     * и выбрасывает исключение, если значение не найдено
+     * @param key принимает значение ключа
+     * @return возвращает значение присвоенное ключу
+     */
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+        }
+        return values.get(key);
+    }
+
+    /**
+     * метод разбивает пару ключ-значение из массива на два значения и записывает их в мапу
+     * @param args принимает массив параметров
+     */
+    private void parse(String[] args) {
+        for (String vol : args) {
+            String[] tmp = vol.substring(1).split("=", 2);
+            values.put(tmp[0], tmp[1]);
+        }
+    }
+
+    /**
+     * проверяет массив параметров на валидность
+     * создает объект класа и запускает метод  parse
+     * @param args массив параметров
+     * @return возвращает ссылку на объект
+     */
+    public static ArgsName of(String[] args) {
+        check(args);
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    /**
+     * Метод проверяет на валидность массив с ключами и значениями
+     * @param args массив с параметрами
+     */
+    private static void check(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        for (String vol : args) {
+            if (!vol.contains("=")) {
+                throw new IllegalArgumentException("Error: This argument '" + vol + "' does not contain an equal sign");
+            }
+            if (!vol.startsWith("-")) {
+                throw new IllegalArgumentException("Error: This argument '" + vol + "' does not start with a '-' character");
+            }
+            String[] temp =  vol.substring(1).split("=");
+            if (!Pattern.compile("^[a-zA-Z0-9]+$").matcher(temp[0]).matches()) {
+                throw new IllegalArgumentException("Error: This argument '" + vol + "' does not contain a key");
+            }
+            if (temp.length == 1) {
+                throw new IllegalArgumentException("Error: This argument '" + vol + "' does not contain a value");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
