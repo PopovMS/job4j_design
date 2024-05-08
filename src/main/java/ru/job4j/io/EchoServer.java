f4a69e8add8a268bdc044b2a9d84e12b9d6b06d7
Misha <m.popov83@gmail.com>
2024-05-08 19:32:18 +0500

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -15,7 +15,13 @@ public class EchoServer {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
-                        if (string.contains("msg=Bye")) {
+                        if (string.contains("msg=Any")) {
+                            output.write("What".getBytes());
+                        }
+                        if (string.contains("msg=Hello")) {
+                            output.write("Hello, dear friend.".getBytes());
+                        }
+                        if (string.contains("msg=Exit")) {
                             server.close();
                         }
                     }
